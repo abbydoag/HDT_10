d85a76a7f99fb37378be52c3da3b5029e0749a78
Abby Donis <abby.donis.a@gmail.com>
2023-05-11 10:56:49 -0600

    metodo para leer .txt (versión inicial)

---
+++ Floyd.java
@@ -0,0 +1,8 @@
+class Floyd{
+    /**
+     * INF = VALOR INFINITO, PARA QUE NO HAYA ERRORES DE TAMAÑO
+     * nV = NO. TOTAL VERTICES
+    */
+    final static int INF = 9999, nV = 7;
+
+}
---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	private static String readData(String dataFile) throws FileNotFoundException {
+		BufferedReader dataScan = new BufferedReader(new FileReader(dataFile));
+		String data = "";
+		
+		try { 
+			data = dataScan.readLine();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(data);		
+		return data;
+	}
+
+	public static void main(String[]args) throws FileNotFoundException{
+        String cities = readData("src/HDT2/datos.txt");
+        String graph = matrixBuilder(cities);
+    }
+    private static String matrixBuilder(String cities){
+        for (int i=0; i < cities.length(); i++) {
+			char character = cities.charAt(i);
+			if(character == ' ') {
+				continue;
+			}
+			if (Character.isDigit(character)){
+				String c = "" + character;
+				int parsedNumber = Integer.parseInt(c);
+			}
+        }
+        return cities;
+    }
+}
