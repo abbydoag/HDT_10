7d3d8db6a0152663db65499485fe1a67e5094ce6
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-05-16 19:51:36 -0600

    se agregó la clase para cargar la matriz

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+        Matrix_colector("adjacency_matrix.txt");
+	}
+
+public static void Matrix_colector(String ruta) { 
+    try {
+        File archivo = new File(ruta);
+        Scanner myReader = new Scanner(archivo);
+        ArrayList<String []> matrix = new ArrayList<String []>();
+        while (myReader.hasNextLine()) {
+            String dato = myReader.nextLine();
+            String[] linea=dato.split(" "); 
+            matrix.add(linea);
+        }
+
+        for (String [] fila : matrix) {
+            for (String elemento : fila) {
+                System.out.print(elemento + " ");
+            }
+            System.out.println(); // Salto de línea después de cada fila
+        }
+    } catch (FileNotFoundException e) {
+        System.out.println("El archivo no existe");
+    }
+
+}
+
+}
