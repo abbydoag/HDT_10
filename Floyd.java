1f7b7c95955b657f03c4e556e56733eb04cbb6f2
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-05-18 08:58:38 -0600

    cambiamos la forma de la maatriz y la funcion que lee dicha matriz :p

--- Floyd.java
+++ Floyd.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 class Floyd<T>{
     /**
      * INF = VALOR INFINITO, PARA QUE NO HAYA ERRORES DE TAMAÑO
      * nV = NO. TOTAL VERTICES
     */
     final static int INF = 9999, nV = 9999; 
+    public ArrayList<Integer[]> separador (ArrayList<String []> matrix ){
+
+    }
 }
