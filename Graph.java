66c11bf48e2b961a3b42438b5ae8663773e1d3c7
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-05-18 09:36:45 -0600

    ola no se prque no cargo todos los archivos al git

---
+++ Graph.java
@@ -0,0 +1,33 @@
+public class Graph implements iGraph {
+
+   
+    private int numVertices;
+    private int[][] matrizAdy;
+
+    public void Grafo(int numVertices) {
+        this.numVertices = numVertices;
+        this.matrizAdy = new int[numVertices][numVertices];
+        // Inicializa la matriz de adyacencia con valores infinitos
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                matrizAdy[i][j] = Integer.MAX_VALUE;
+            }
+        }
+    }
+
+    // Método para agregar una arista al grafo
+    public void agregarArista(int origen, int destino, int peso) {
+        matrizAdy[origen][destino] = peso;
+    }
+
+    // Método para obtener el número de vértices del grafo
+    public int getNumVertices() {
+        return numVertices;
+    }
+
+    // Método para obtener la matriz de adyacencia del grafo
+    public int[][] getMatrizAdy() {
+        return matrizAdy;
+    }
+    
+}
