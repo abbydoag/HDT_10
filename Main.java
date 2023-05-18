e847d1f33d421c3888e06af6d410c17604b0e9eb
abbydoag <86386562+abbydoag@users.noreply.github.com>
2023-05-17 20:05:54 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
         Matrix_colector("adjacency_matrix.txt");
+		
 	}
-
-public static void Matrix_colector(String ruta) { 
+	public static void Matrix_colector(String ruta) { 
     try {
         File archivo = new File(ruta);
-        Scanner myReader = new Scanner(archivo);
-        ArrayList<String []> matrix = new ArrayList<String []>();
-        while (myReader.hasNextLine()) {
-            String dato = myReader.nextLine();
-            String[] linea=dato.split(" "); 
-            matrix.add(linea);
-        }
+        try (Scanner myReader = new Scanner(archivo)) {
+			ArrayList<String []> matrix = new ArrayList<String []>();
+			while (myReader.hasNextLine()) {
+			    String dato = myReader.nextLine();
+			    String[] linea=dato.split(" "); 
+			    matrix.add(linea);
+			}
 
-        for (String [] fila : matrix) {
-            for (String elemento : fila) {
-                System.out.print(elemento + " ");
-            }
-            System.out.println(); // Salto de línea después de cada fila
-        }
+			for (String [] fila : matrix) {
+			    for (String elemento : fila) {
+			        System.out.print(elemento + " ");
+			    }
+			    System.out.println(); // Salto de línea después de cada fila
+			}
+		}
     } catch (FileNotFoundException e) {
         System.out.println("El archivo no existe");
     }
-
-}
-
+	Floyd floyd = new Floyd<>();
+	}
 }
