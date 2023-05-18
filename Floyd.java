5e4a9579ea7162ba90e0a11942f6859eef3c9b05
Abby Donis <abby.donis.a@gmail.com>
2023-05-18 12:08:12 -0600

    llamada de grafo

--- Floyd.java
+++ Floyd.java
@@ -4,9 +4,13 @@ class Floyd<T>{
     /**
      * INF = VALOR INFINITO, PARA QUE NO HAYA ERRORES DE TAMAÑO
      * nV = NO. TOTAL VERTICES
+     * 99999 = INFINITO
     */
     final static int INF = 9999, nV = 9999; 
     public ArrayList<Integer[]> separador (ArrayList<String []> matrix ){
-
+        Graph graph = new Graph();
+        int numVertices = 3;
+        graph.Grafo(numVertices);
+        return null;
     }
 }
