a71e0df0320a504878d1532c79751e58f147a4b8
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2023-05-18 09:27:47 -0600

    ola, sigo aplicando arista

--- iArista.java
+++ iArista.java
@@ -1,10 +1,14 @@
 public interface iArista {
-    public void Arista(String ciudadDestino, int tiempoNormal, int tiempoNieve) ;
+    public void Arista(String ciudadDestino, int tiempoNormal,int tiempoLluvia, int tiempoNieve, int tiempoTormenta) ;
 
     public String getCiudadDestino();
 
     public int getTiempoNormal();
 
+    public int getTiempoLluvia();
+
     public int getTiempoNieve();
+
+    public int getTiempoTormenta();
 
 }
